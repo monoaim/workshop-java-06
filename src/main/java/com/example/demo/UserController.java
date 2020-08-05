4b76575230e61f93d21b8cf1899d219aa3d480ef
Araya Siriadun <araya.siriadun@gmail.com>
2020-08-05 16:58:21 +0700

    Step 5: Pagination

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -2,14 +2,17 @@ package com.example.demo;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.PageRequest;
 
 @RestController
 public class UserController {
@@ -20,15 +23,19 @@ public class UserController {
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(name = "item_per_page", defaultValue = "10") int itemPerPage) {
         PagingResponse pagingResponse = new PagingResponse(page, itemPerPage);
-        List<UsersResponse> usersResponseList = this.getSampleUsers(100);
-        usersResponseList = usersResponseList.subList((page - 1) * itemPerPage, page * itemPerPage);
+        List<UsersResponse> usersResponseList = new ArrayList<>();
+        Page<User> users = userRepository.findAll(PageRequest.of(page, itemPerPage));
+        for (User user : users.getContent()) {
+            usersResponseList.add(new UsersResponse(user.getId(), user.getName(), user.getAge()));
+        }
         pagingResponse.setUsersResponse(usersResponseList);
         return pagingResponse;
     }
 
     @GetMapping("/users/{id}")
     public UsersResponse getUserById(@PathVariable int id) {
-        return new UsersResponse(id, String.format("User %d", id), new Random().nextInt(100) + 1);
+        Optional<User> users = userRepository.findById(id);
+        return new UsersResponse(users.get().getId(), users.get().getName(), users.get().getAge());
     }
 
     @PostMapping("/users")
--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -2,5 +2,6 @@ package com.example.demo;
 
 import com.example.demo.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {}
+public interface UserRepository extends PagingAndSortingRepository<User, Integer> {}
